25e38fa3c97ffb5950b6f57dc2bc09f69da14394
erick-rios <erickrg2700@ciencias.unam.mx>
2024-11-08 22:45:40 -0600

    Add HTML map for 2026 World Cup stadiums in Mexico and variable costs data

--- src/visualize.py
+++ src/visualize.py
@@ -2,17 +2,38 @@
 
 import folium
 
-# Coordinates of the Mexican stadiums to be used in the 2026 World Cup
+# Cordinates of the stadiums
 estadios_mexico = {
     "Estadio Azteca (CDMX)": [19.3029, -99.1506],
     "Estadio Akron (Guadalajara)": [20.6755, -103.462],
     "Estadio BBVA (Monterrey)": [25.6687, -100.3116]
 }
 
-# Crear a map centered in Mexico
+# Cordinates of the breweries
+plantas_cerveza = {
+    "Planta Grupo Modelo (CDMX)": [19.4401, -99.1255],
+    "Planta Heineken (Monterrey)": [25.7304, -100.3147],
+    "Planta Grupo Modelo (Guadalajara)": [20.725, -103.3307]
+}
+
+# Asign a supply chain to each stadium
+abastecimiento = {
+    "Estadio Azteca (CDMX)": ["Planta Grupo Modelo (CDMX)"],
+    "Estadio Akron (Guadalajara)": ["Planta Grupo Modelo (Guadalajara)"],
+    "Estadio BBVA (Monterrey)": ["Planta Heineken (Monterrey)"]
+}
+
+# Create a map centered in Mexico
 mapa_mexico = folium.Map(location=[23.6345, -102.5528], zoom_start=5)
 
-# Add a marker for each stadium
+# Colour code for the breweries
+colores = {
+    "Planta Grupo Modelo (CDMX)": "green",
+    "Planta Heineken (Monterrey)": "red",
+    "Planta Grupo Modelo (Guadalajara)": "purple"
+}
+
+# Add markers for the stadiums
 for estadio, coords in estadios_mexico.items():
     folium.Marker(
         location=coords,
@@ -20,7 +41,15 @@ for estadio, coords in estadios_mexico.items():
         icon=folium.Icon(color="blue", icon="info-sign")
     ).add_to(mapa_mexico)
 
-# Save maps as HTML file
-mapa_mexico.save("../images/mapa_estadios_mundial_2026_mexico.html")
+# Add markers for the breweries
+for planta, coords in plantas_cerveza.items():
+    folium.Marker(
+        location=coords,
+        popup=planta,
+        icon=folium.Icon(color=colores[planta], icon="industry")
+    ).add_to(mapa_mexico)
 
-print("Mapa guardado como 'mapa_estadios_mundial_2026_mexico.html'")
+# Guardar el mapa en un archivo HTML
+mapa_mexico.save("../images/mapa_estadios_plantas_cerveza_mundial_2026_mexico.html")
+
+print("Mapa guardado como 'mapa_estadios_plantas_cerveza_mundial_2026_mexico.html'")
